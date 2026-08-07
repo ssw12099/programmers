a255421e89a92758669026126eb04e928e9835b0
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-07 14:46:56 +0900

    [level 1] Title: 시저 암호, Time: 10.74 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,8 +3,7 @@ class Solution {
         String answer = "";
         for(int i=0;i<s.length();i++){
             char tmp = s.charAt(i);
-            answer += (tmp==' ')?" ":(tmp>='a')?
-                (char)((tmp-'a'+n)%26+'a')+"":(char)((tmp-'A'+n)%26+'A')+"" ;
+            answer += (tmp==' ')?" ":(tmp>='a')?(char)((tmp-'a'+n)%26+'a')+"":(char)((tmp-'A'+n)%26+'A')+"" ;
         }
         return answer;
     }
