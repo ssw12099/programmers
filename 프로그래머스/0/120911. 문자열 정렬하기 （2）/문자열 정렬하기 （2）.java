500d1270fc875538e828b8816d61a5d946544454
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-12 14:22:25 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.30 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string) {
+        char[] answer = my_string.toLowerCase().toCharArray();
+        Arrays.sort(answer);
+        return new String(answer);
+    }
+}
