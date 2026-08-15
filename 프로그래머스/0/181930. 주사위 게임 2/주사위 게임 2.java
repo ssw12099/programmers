3046f301ac41064b8fdfd74a4089e83dfdb6e346
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-15 15:56:21 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,5 +1,9 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        return ((a*2-b-c)==0)?((a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)):(a==b||a==c||b==c)?((a+b+c)*(a*a+b*b+c*c)):(a+b+c);
+        return ((a-b)*(a-b) + (b-c)*(b-c) + (c-a)*(c-a) == 0)
+            ? (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
+            : ((a-b)*(b-c)*(c-a) == 0)
+            ? (a+b+c)*(a*a+b*b+c*c)
+            : (a+b+c);
     }
 }
