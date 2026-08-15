8943930689f86e6273c18d1c119051a23095b55b
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-15 16:00:33 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int solution(int[] common) {
+        return (common[common.length-1]-common[common.length-2]==common[1]-common[0])?(common[common.length-1]+common[1]-common[0]):(common[common.length-1]*common[1]/common[0]);
+    }
+}
