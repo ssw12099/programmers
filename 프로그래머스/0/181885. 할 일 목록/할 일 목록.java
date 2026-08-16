d77750cdacdc52d9e009934c1b1ee21194ac9c1e
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-16 14:28:02 +0900

    [level 0] Title: 할 일 목록, Time: 2.74 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -2,7 +2,6 @@ class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
         String sb = "";
         for(int i=0;i<todo_list.length;sb+=(finished[i++])?"":todo_list[i-1]+" ");
-        String[] answer = sb.split(" ");
-        return answer;
+        return sb.split(" ");
     }
 }
