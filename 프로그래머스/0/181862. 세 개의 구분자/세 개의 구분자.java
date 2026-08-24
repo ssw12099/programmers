1c96f0c524b0934e150ba55a706367219b938b5f
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-24 15:48:10 +0900

    [level 0] Title: 세 개의 구분자, Time: 58.83 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,8 +1,6 @@
 class Solution {
     public String[] solution(String myStr) {
-        myStr = myStr.replaceAll("[abc]"," ").replaceAll("\\s+"," ");
-        String[] answer = myStr.trim().split(" ");
-        if(answer[0].equals(""))answer[0]="EMPTY";
-        return answer;
+        String[] answer = myStr.replaceAll("[abc]"," ").replaceAll("\\s+"," ").trim().split(" ");
+        return (answer[0].equals(""))?new String[]{"EMPTY"}:answer;
     }
 }
