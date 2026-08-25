7b3ee125a91479c6e8c028d2af8ec2dae6598bc1
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-25 19:31:31 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int a=-1,b=-1,c=-1;
+        for(int i=0;i<rank.length;i++){
+            if(a==-1&&attendance[i])a=i;
+            if(a!=-1&&rank[a]>rank[i]&&attendance[i])a=i;
+        }
+        for(int i=0;i<rank.length;i++){
+            if(b==-1&&a!=i&&attendance[i])b=i;
+            if(b!=-1&&a!=i&&rank[b]>rank[i]&&attendance[i])b=i;
+        }
+        for(int i=0;i<rank.length;i++){
+            if(c==-1&&a!=i&&b!=i&&attendance[i])c=i;
+            if(c!=-1&&a!=i&&b!=i&&rank[c]>rank[i]&&attendance[i])c=i;
+        }
+        
+        int answer = 10000*a+100*b+c;
+        return answer;
+    }
+}
