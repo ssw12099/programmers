fc686cc04d0f9a9db5dbd688ed5083534d688416
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-08-26 18:50:04 +0900

    [level 0] Title: 다항식 더하기, Time: 6.68 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String polynomial) {
+        String[] str = polynomial.replaceAll(" \\+ "," ").split(" ");
+        int x=0,c=0;
+        for(int i=0;i<str.length;i++){
+            if(str[i].charAt(str[i].length()-1)=='x'){
+                if((str[i].length()==1))x++;
+                else x+=(Integer.parseInt(str[i].replace("x","")));
+            }else c+=Integer.parseInt(str[i]);
+        }
+        String answer = ((x==0)?"":((x==1)?"x":x+"x"))+((x!=0&&c!=0)?" + ":"")+((c==0)?"":""+c);
+        return answer;
+    }
+}
