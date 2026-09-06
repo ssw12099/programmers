1c48a3ba0f962d0cb08cc4aaa0ccf40b0faf231f
ssw12099 <44778894+ssw12099@users.noreply.github.com>
2026-09-06 13:39:35 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.44 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        return arr[0]+" "+arr[arr.length-1];
+    }
+}
